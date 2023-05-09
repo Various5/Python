f9d275b9d3aeac1cee7a91acaa6c40756770653a
Various5 <44600128+Various5@users.noreply.github.com>
2023-05-09 19:19:20 +0200

    Create wwwcrawler_url.py

---
+++ wwwcrawler_url.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+def get_links(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    links = []
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        if href.startswith('https'):
+            links.append(href)
+    return links
+
+def crawl_website(url):
+    links = set()
+    visited = set()
+    links.add(url)
+    while len(links) > 0:
+        current_url = links.pop()
+        if current_url in visited:
+            continue
+        print('Crawling:', current_url)
+        current_links = get_links(current_url)
+        visited.add(current_url)
+        for link in current_links:
+            if link not in visited:
+                links.add(link)
+    return visited
+
+if __name__ == '__main__':
+    url = input('Enter a website URL: ')
+    visited = crawl_website(url)
+    json_data = json.dumps(list(visited))
+    with open('website_links.json', 'w') as f:
+        f.write(json_data)
