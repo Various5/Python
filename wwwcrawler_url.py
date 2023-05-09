4626b46163092940a9a32083e917b44f529102a3
Various5 <44600128+Various5@users.noreply.github.com>
2023-05-09 19:32:34 +0200

    Update wwwcrawler_url.py
    removed following external urls ^^

--- wwwcrawler_url.py
+++ wwwcrawler_url.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+from urllib.parse import urlparse
 
 def get_links(url):
     response = requests.get(url)
@@ -8,28 +9,31 @@ def get_links(url):
     links = []
     for link in soup.find_all('a'):
         href = link.get('href')
-        if href is not None and href.startswith(('http', 'https')):
+        if href is not None and (href.startswith('http') or href.startswith('https')):
             links.append(href)
-    for link in soup.find_all('link'):
-        href = link.get('href')
-        if href is not None and href.startswith(('http', 'https')):
-            links.append(href)
-    for script in soup.find_all('script'):
-        src = script.get('src')
-        if src is not None and src.startswith(('http', 'https')):
-            links.append(src)
     return links
 
 def crawl_website(url):
     links = set()
     visited = set()
     links.add(url)
+    domain = urlparse(url).netloc
     while len(links) > 0:
         current_url = links.pop()
         if current_url in visited:
             continue
+        if not current_url.startswith('http'):
+            current_url = 'http://' + current_url
+        if not current_url.startswith('https'):
+            current_url = 'https://' + current_url
+        if urlparse(current_url).netloc != domain:
+            continue
         print('Crawling:', current_url)
-        current_links = get_links(current_url)
+        try:
+            current_links = get_links(current_url)
+        except requests.exceptions.ConnectionError:
+            print('Skipping:', current_url)
+            continue
         visited.add(current_url)
         for link in current_links:
             if link not in visited:
