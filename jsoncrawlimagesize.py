098119b37f8cea967099d6b82e8335dbd6492d17
Various5 <44600128+Various5@users.noreply.github.com>
2023-05-09 21:37:39 +0200

    a

--- jsoncrawlimagesize.py
+++ jsoncrawlimagesize.py
@@ -3,7 +3,7 @@ import json
 import csv
 from PIL import Image
 from io import BytesIO
-import re  # Add this line to import the 're' module
+import re
 
 # Set up CSV file
 with open('image_sizes.csv', 'w', newline='') as csvfile:
@@ -14,33 +14,45 @@ with open('image_sizes.csv', 'w', newline='') as csvfile:
 with open('urls.json') as json_file:
     urls = json.load(json_file)
 
+# Set request timeout limit to 10 seconds
+timeout = 10
+
 # Iterate through each URL
 for url in urls:
-    # Get page content
-    response = requests.get(url)
-    page_content = response.content
+    try:
+        print("Processing URL:", url)
 
-    # Get all images on page
-    images = []
-    img_formats = ["jpg", "jpeg", "png", "gif"]
-    for fmt in img_formats:
-        images.extend(re.findall(r'(?i)([^<>\"\']*?\.(?:%s))' % fmt, str(page_content)))
+        # Get page content
+        response = requests.get(url, timeout=timeout)
+        page_content = response.content
 
-    # Get size of each image
-    for image in images:
-        try:
-            img_data = requests.get(image).content
-            img = Image.open(BytesIO(img_data))
-            img_size = len(img_data) / 1024.0
+        # Get all images on page
+        images = []
+        img_formats = ["jpg", "jpeg", "png", "gif"]
+        for fmt in img_formats:
+            images.extend(re.findall(r'(?i)([^<>\"\']*?\.(?:%s))' % fmt, str(page_content)))
 
-            # Write to CSV file
-            with open('image_sizes.csv', 'a', newline='') as csvfile:
-                writer = csv.writer(csvfile)
-                if img_size >= 1024:
-                    img_size = img_size / 1024.0
-                    writer.writerow([image, '{:.2f} MB'.format(img_size)])
-                else:
-                    writer.writerow([image, '{:.2f} KB'.format(img_size)])
+        # Get size of each image
+        for image in images:
+            try:
+                img_data = requests.get(image, timeout=timeout).content
+                img = Image.open(BytesIO(img_data))
+                img_size = len(img_data) / 1024.0
 
-        except:
-            print("Could not retrieve size for image: ", image)
+                # Write to CSV file
+                with open('image_sizes.csv', 'a', newline='') as csvfile:
+                    writer = csv.writer(csvfile)
+                    if img_size >= 1024:
+                        img_size = img_size / 1024.0
+                        writer.writerow([image, '{:.2f} MB'.format(img_size)])
+                    else:
+                        writer.writerow([image, '{:.2f} KB'.format(img_size)])
+
+                print("Processed image:", image, "| Size:", '{:.2f}'.format(img_size), "KB")
+
+            except:
+                print("Could not retrieve size for image: ", image)
+
+    except requests.exceptions.Timeout:
+        print("Request timed out for URL: ", url)
+        continue
