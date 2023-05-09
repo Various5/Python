d5abf727c50f37bbf4e9184ec381850cd11c0fb2
Various5 <44600128+Various5@users.noreply.github.com>
2023-05-09 20:31:32 +0200

    a

--- wwwcrawler_url.py
+++ wwwcrawler_url.py
@@ -19,31 +19,34 @@ def crawl_website(url, timeout, save_count):
     visited = set()
     links.add(url)
     domain = urlparse(url).netloc
-    for i, link in enumerate(list(links)):
-        if i % save_count == 0 and i > 0:
-            json_data = json.dumps(list(visited))
-            with open('website_links.json', 'w') as f:
-                f.write(json_data)
-        if i % timeout == 0 and i > 0:
-            time.sleep(2)
-        if link in visited:
-            continue
-        if not link.startswith('http'):
-            link = 'http://' + link
-        if not link.startswith('https'):
-            link = 'https://' + link
-        if urlparse(link).netloc != domain:
-            continue
-        try:
-            current_links = get_links(link)
-        except requests.exceptions.ConnectionError:
-            print('Skipping:', link)
-            continue
-        visited.add(link)
-        print(f'Visited: {len(visited)}', end='\r')
-        for sub_link in current_links:
-            if sub_link not in visited:
-                links.add(sub_link)
+    while len(links) > 0:
+        links_copy = links.copy()
+        for link in links_copy:
+            if len(visited) % save_count == 0 and len(visited) > 0:
+                json_data = json.dumps(list(visited))
+                with open('website_links.json', 'w') as f:
+                    f.write(json_data)
+            if len(visited) % timeout == 0 and len(visited) > 0:
+                time.sleep(2)
+            if link in visited:
+                continue
+            if not link.startswith('http'):
+                link = 'http://' + link
+            if not link.startswith('https'):
+                link = 'https://' + link
+            if urlparse(link).netloc != domain:
+                continue
+            try:
+                current_links = get_links(link)
+            except requests.exceptions.ConnectionError:
+                print('Skipping:', link)
+                continue
+            visited.add(link)
+            print(f'Visited: {len(visited)}', end='\r')
+            for sub_link in current_links:
+                if sub_link not in visited:
+                    links.add(sub_link)
+            links.remove(link)
     json_data = json.dumps(list(visited))
     with open('website_links.json', 'w') as f:
         f.write(json_data)
