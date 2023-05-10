413d5d1b2a9095115ea1b9cef6b1f8f4cca9b32c
Various5 <44600128+Various5@users.noreply.github.com>
2023-05-10 12:58:31 +0200

    Update jsoncrawlimagesize.py
    prevent doubles

--- jsoncrawlimagesize.py
+++ jsoncrawlimagesize.py
@@ -27,14 +27,19 @@ def get_image_size(image):
         parser = ImageFile.Parser()
         parser.feed(img_data)
         img_size = len(img_data) / 1024.0
+        img_name = re.search(r'/([^/]+\.(jpg|jpeg|png|gif))$', image, re.I).group(1)
+        if img_name in processed_images:
+            print("Skipping image with duplicate name:", img_name)
+            return
         with open('image_sizes.csv', 'a', newline='') as csvfile:
             writer = csv.writer(csvfile)
             if img_size >= 1024:
                 img_size = img_size / 1024.0
-                writer.writerow([image, '{:.2f} MB'.format(img_size)])
+                writer.writerow([img_name, '{:.2f} MB'.format(img_size)])
             else:
-                writer.writerow([image, '{:.2f} KB'.format(img_size)])
-        print("Processed image:", image, "| Size:", '{:.2f}'.format(img_size), "KB")
+                writer.writerow([img_name, '{:.2f} KB'.format(img_size)])
+        processed_images.add(img_name)
+        print("Processed image:", img_name, "| Size:", '{:.2f}'.format(img_size), "KB")
     except:
         print("Could not retrieve size for image: ", image)
 
@@ -55,6 +60,7 @@ for url in urls:
             images.extend(re.findall(r'(?i)([^<>\"\']*?\.(?:%s))' % fmt, str(page_content)))
 
         # Process each image
+        processed_images = set()
         for image in images:
             get_image_size(image)
             time.sleep(1) # Add a delay between requests
