439b9b7ad09d3893eb5bf3893cee6aa8ed7c65f7
Various5 <44600128+Various5@users.noreply.github.com>
2023-10-31 19:08:28 +0100

    Update ftpcrawler.py

--- ftpcrawler.py
+++ ftpcrawler.py
@@ -24,15 +24,28 @@ print(f"Connected to FTP server: {FTP_HOST}")
 ftp.cwd(FTP_MOVIE_FOLDER)
 print(f"Changed directory to {FTP_MOVIE_FOLDER}")
 
+# Function to split path into its constituent parts
+def split_path(path):
+    # Split the path to get folder and file name separately
+    parts = path.rstrip("/").split("/")
+    filename = parts[-1] if parts[-1] else None
+    foldername = parts[-2] if len(parts) > 1 else None
+    path = "/".join(parts[:-1])
+    return path, foldername, filename
+
 # This function will be called for each file in the directory and subdirectories
 def add_to_csv(ftp_conn, path, csv_writer, depth):
     if depth > MAX_DEPTH:
         print(f"Reached maximum depth of {MAX_DEPTH} in directory: {path}")
         return
 
-    items = ftp_conn.nlst(path)
+    try:
+        items = ftp_conn.nlst(path)
+    except ftplib.error_perm as e:
+        print(f"Access denied to directory: {path}, skipping...")
+        return
+
     for item in items:
-        # Create the full path
         full_path = os.path.join(path, item) if path != "/" else "/" + item
         try:
             # Try to change to directory to check if it's a directory or a file
@@ -43,25 +56,20 @@ def add_to_csv(ftp_conn, path, csv_writer, depth):
             # Go back up to the parent directory
             ftp_conn.cwd("..")
         except ftplib.error_perm as e:
-            # It's a file, get its size and write to CSV
-            try:
-                size = ftp_conn.size(full_path)
-                csv_writer.writerow([full_path, size])
-                print(f"File added: {full_path} Size: {size} bytes")
-            except ftplib.error_perm as e:
-                # Unable to get the size for some reason, write without size
-                csv_writer.writerow([full_path, 'unknown'])
-                print(f"File added (size unknown): {full_path}")
+            # It's a file, write path, folder, and filename to CSV
+            file_path, folder_name, filename = split_path(full_path)
+            csv_writer.writerow([file_path, folder_name, filename])
+            print(f"File added: {full_path}")
 
 # Open the CSV file to write
 with open(CSV_FILENAME, 'w', newline='') as csvfile:
     csvwriter = csv.writer(csvfile)
     # Write the header
-    csvwriter.writerow(["Filename", "Filesize (bytes)"])
-    # Start the recursive function to add filenames and file sizes to CSV with initial depth 0
+    csvwriter.writerow(["Path", "Folder", "Filename"])
+    # Start the recursive function to add file details to CSV with initial depth 0
     add_to_csv(ftp, FTP_MOVIE_FOLDER, csvwriter, 0)
 
 # Close the FTP connection
 ftp.quit()
-print(f"CSV file '{CSV_FILENAME}' has been created with the list of movie filenames and their file sizes.")
+print(f"CSV file '{CSV_FILENAME}' has been created with the list of files.")
 print("FTP connection closed.")
